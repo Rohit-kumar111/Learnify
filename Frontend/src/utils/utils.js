f19217b5ace408f6aa132dedbc7b7fc89bbb3bf9
Rohit Kumar <kumarrohit45650@gmail.com>
2025-08-16 15:59:53 +0530

    again changed url

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,2 +1,2 @@
 // export const BACKEND_URL="http://localhost:3000/api/v1"
-export const BACKEND_URL="https://learnify-569p.onrender.com"
+export const BACKEND_URL="https://learnify-569p.onrender.com/api/v1"
